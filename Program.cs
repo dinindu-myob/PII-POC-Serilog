50186cb942797aac97636c7a404be2fc6beda9f8
dinindu-myob <Dinindu.Fernando@myob.com>
2024-08-06 14:45:11 +1000

    Minor changes Update ReadMe

--- Program.cs
+++ Program.cs
@@ -16,9 +16,11 @@ Customer customer = new Customer
 Log.Logger = new LoggerConfiguration()
     //Using Custom enricher
     //.Enrich.With(new SensitiveDataEnricher())
-    //
-    //Using Serilog.Enrichers.Sensitive
+    
+    //Using Serilog.Enrichers.Sensitive Default masking
     //.Enrich.WithSensitiveDataMasking()
+
+    //Using Serilog.Enrichers.Sensitive Custom masking
     .Enrich.WithSensitiveDataMasking(
         options =>
         {
@@ -33,9 +35,10 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .CreateLogger();
 
-
+Console.WriteLine("Logging Customer Object directly");
 Log.Information(@"Customer Object {@Customer}", customer);
 
+Console.WriteLine("Logging Customer Info as a formatted string");
 Log.Information($"Customer Info: ID: {customer.Id}, Name: {customer.Name}, " +
                 $"Email: {customer.Email}, Serial Number: {customer.SerialNumber}");
 
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 ## PII POC Serilog
 This is a smiple POC done to try out implementing Serilog custom enrichers using Serilogs ILogEventEnricher and also nuget package https://github.com/serilog-contrib/Serilog.Enrichers.Sensitive
+
+### Prerequisites
+- .NET 8.0
+
+### How to run
+
+Run the follwoing command in the root directory of the project
+```
+dotnet run
+```
+
+Sample Output:
+```
+Logging Customer Object directly
+[14:33:37 INF] Customer Object {"Id": 1, "Name": "John Doe", "Email": "t***p@example.com", "SerialNumber": "1**********2"}
+Logging Customer Info as a formatted string
+[14:33:37 INF] Customer Info: ID: 1, Name: John Doe, Email: t***p@example.com, Serial Number: 1**********2
+
+```
+
